94c2fce710cfeebfe70eafedd8dcf99add811726
root <root@ip-172-31-8-9.ap-south-1.compute.internal>
2018-12-09 06:05:36 +0000

    corrected the script

--- jenkins/files/install_jenkins.sh
+++ jenkins/files/install_jenkins.sh
@@ -1,5 +1,5 @@
 cd /opt/
-yum install wget
+apt-get install wget
 sudo wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 sudo java -jar jenkins.war --httpPort=80 > /var/log/jenkins.log 2>&1 &
 cat /root/.jenkins/secrets/initialAdminPassword
